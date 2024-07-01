a7bbb92fc7dda0258207d8e133d9ea79517b7af3
Wontak Kim <wontak0103@gmail.com>
2024-07-01 20:33:05 +0900

    Add flush gradients every before backward call

--- micrograd/main.py
+++ micrograd/main.py
@@ -12,10 +12,11 @@ if __name__ == "__main__":
 
     for round in range(100):
         ypred = [n(x) for x in xs]
-
-        print(f"{round} round : {ypred}")
-
         loss = sum([(yout - ygt) ** 2 for ygt, yout in zip(ys, ypred)])
+
+        print(f"{round} round ({loss.data}) : {ypred}")
+        for p in n.parameters():
+            p.grad = 0.0
         loss.backward()
 
         for p in n.parameters():
